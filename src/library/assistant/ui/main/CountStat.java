22ebe18c9f59cf0ac1a6ccd70f57b182ac597462
Clinton Yeboah <clintonyeb@gmail.com>
2020-03-06 16:46:13 -0800

    more revisions

---
+++ src/library/assistant/ui/main/CountStat.java
@@ -0,0 +1,27 @@
+package library.assistant.ui.main;
+
+public class CountStat {
+    private String field;
+    private long count;
+
+    public CountStat(String field, long count) {
+        this.field = field;
+        this.count = count;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
