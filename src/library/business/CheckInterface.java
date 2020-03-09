7ab3afdeb429647f047a5dfa6eee786cfce5a591
EllyB'ge <ell86bu@gmail.com>
2020-03-09 11:54:40 -0500

    cODE REFACTORING

--- src/library/business/CheckInterface.java
+++ src/library/business/CheckInterface.java
@@ -1,10 +1,16 @@
 package library.business;
 
 import library.exceptions.CheckException;
+import library.model.CheckoutRecord;
+import library.model.CheckoutRecordEntry;
 
 public interface CheckInterface {
 
     void checkOutBook(String isbn, String memberId) throws CheckException;
 
     void checkInBook(String isbn, int copyNum, String memberId) throws CheckException;
+    
+    CheckoutRecord getCheckoutRecord(String memberId);
+    
+    CheckoutRecordEntry[] getCheckoutRecordEntries(String memberId);
 }
